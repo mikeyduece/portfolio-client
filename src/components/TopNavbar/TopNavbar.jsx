884a5ad84359203c6fb1b021d05b8552821206a3
Mike Heft <mikeheft@gmail.com>
2020-04-05 15:49:39 -0600

    Work on revelaing hexagons

--- src/components/TopNavbar/TopNavbar.jsx
+++ src/components/TopNavbar/TopNavbar.jsx
@@ -17,9 +17,9 @@ const TopNavbar = props => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Link to='/about' className={`nav-link ${isActive('/about')}`} role='button'>About</Link>
-          <Link to='/projects' className={`nav-link ${isActive('/projects')}`} role='button'>Projects</Link>
-          <Link to='/contact-me' className={`nav-link ${isActive('/contact-me')}`} role='button'>Contact Me</Link>
+          <Link to='/about' className={ `nav-link ${ isActive('/about') }` } role='button'>About</Link>
+          <Link to='/projects' className={ `nav-link ${ isActive('/projects') }` } role='button'>Projects</Link>
+          <Link to='/contact-me' className={ `nav-link ${ isActive('/contact-me') }` } role='button'>Contact Me</Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
