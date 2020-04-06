b597f335c28c6835fa03ac89267045903f9f7bb4
Mike Heft <mikeheft@gmail.com>
2020-04-06 08:02:53 -0600

    Work on styling hihglights

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,31 +1,34 @@
 // Dependencies
 import './About.scss'
-import React          from 'react'
-import Flip           from 'react-reveal/Flip'
-import Hexagon        from 'react-hexagon'
-import Img            from 'react-image'
-import { withNavbar } from '../withNavbar'
+import React, { useEffect } from 'react'
+import Flip                 from 'react-reveal/Flip'
+import Img                  from 'react-image'
+import { withNavbar }       from '../withNavbar'
 
 const About = () => {
   return (
-    <div className='about d-flex flex-column align-items-center'>
-        <Flip right cascade>
-          <Hexagon style={ { stroke: 'red' } }
-                   backgroundImage='https://portfolio-client-development.s3.amazonaws.com/images/me.jpeg'
-                   backgroundScale={ 1.05 }
-          />
-        </Flip>
+    <div className='about d-flex flex-column align-items-center justify-content-between'>
+      <h1 className='mt-3 mb-5' id='heading'>About</h1>
+      <div className='d-flex flex-row'>
+        <Flip right cascade>first cell</Flip>
+        <Flip right cascade>second cell</Flip>
+        <Flip right cascade>third cell</Flip>
+        <Flip right cascade>fourth cell</Flip>
+      </div>
       <div
-        className='d-flex flex-column flex-md-row justify-content-center justify-content-md-around align-items-center w-100'>
-        <div className='d-flex flex-column justify-content-md-between align-items-center align-items-md-start'>
+        className='d-flex flex-column-reverse flex-md-row justify-content-center justify-content-md-around align-items-center w-100'>
+        <div className='d-flex flex-column justify-content-between align-items-center align-items-md-start'>
           <Img src='https://portfolio-client-development.s3.amazonaws.com/images/me.jpeg'
                className='rounded-circle w-50' />
           <div>This is my bio!!!!</div>
           <div>Misc</div>
+
         </div>
 
         <div>Skill Chart</div>
+
       </div>
+
     </div>
   )
 }
