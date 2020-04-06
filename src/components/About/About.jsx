46991f81146f722ec4e072890972f363b11be0f2
Mike Heft <mikeheft@gmail.com>
2020-04-06 15:18:06 -0600

    Add loading indicator when image not laoded yet

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,24 +7,32 @@ import TalkingPointList from '../Welcome/TalkingPoint/TalkingPointList'
 
 const About = () => {
   return (
-    <div className='about d-flex flex-column align-items-center justify-content-between'>
+    <div className='about d-flex flex-column align-items-center justify-content-between container'>
       <h1 className='mt-3 mb-5' id='heading'>About</h1>
       <TalkingPointList />
+      <section>
+        <div
+          className='d-flex flex-column-reverse flex-md-row justify-content-center justify-content-md-between align-items-center mx-4'>
+          <div className='d-flex flex-column justify-content-between align-items-center align-items-md-start'
+               id='profile-container'>
+            <Img src='https://portfolio-client-development.s3.amazonaws.com/images/me.jpeg'
+                 className='rounded-circle w-50'
+                 loader={ <div id='loader-container'>
+                   <div className="spinner-border" style={ { width: '3rem', height: '3rem' } } role="status">
+                     <span className="sr-only">Loading...</span>
+                   </div>
+                 </div> }
+            />
+            <div className='text-center'>
+              <p>This is my bio!</p>
+              <p>Misc description</p>
+            </div>
 
-      <div
-        className='d-flex flex-column-reverse flex-md-row justify-content-center justify-content-md-around align-items-center w-100'>
-        <div className='d-flex flex-column justify-content-between align-items-center align-items-md-start'>
-          <Img src='https://portfolio-client-development.s3.amazonaws.com/images/me.jpeg'
-               className='rounded-circle w-50' />
-          <div>This is my bio!!!!</div>
-          <div>Misc</div>
+          </div>
 
+          <div>Skill Chart</div>
         </div>
-
-        <div>Skill Chart</div>
-
-      </div>
-
+      </section>
     </div>
   )
 }
