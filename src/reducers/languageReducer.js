a3adf09377b6429d298d7b9d28d1d3b7cdcd445f
Mike Heft <mikeheft@gmail.com>
2020-04-07 10:05:17 -0600

    Add Lodash

--- src/reducers/languageReducer.js
+++ src/reducers/languageReducer.js
@@ -1,10 +1,10 @@
-import {FETCH_LANGUAGES} from '../actions/types'
+import _                   from 'lodash'
+import { FETCH_LANGUAGES } from '../actions/types'
 
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_LANGUAGES:
-      console.log(action.payload)
-      return {...state, streams: action.payload}
+      return { ...state, ..._.mapKeys(action.payload, 'id') }
     default:
       return state
   }
