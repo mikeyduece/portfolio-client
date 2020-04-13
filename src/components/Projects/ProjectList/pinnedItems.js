69f10d3298b29b2dc6306f65a1959c5071994f07
Mike Heft <mikeheft@gmail.com>
2020-04-13 10:38:52 -0600

    Add static items to show for projects until graphql-client fixes issue with requiring schema as argument to initiallize client

---
+++ src/components/Projects/ProjectList/pinnedItems.js
@@ -0,0 +1,39 @@
+// TODO: Replace with api call to get pinnedItems once graphql-client has fixed it's issue with missing keyword error.
+export default [
+  {
+    name: 'Bread Shop',
+    url: 'https://github.com/mikeyduece/bread_shop',
+    description: 'Rails API that takes in recipe information and calculates the baker\'s ' +
+      'percentage to return a family for that recipe. With the family set, a user can see a basic procedure to make and ' +
+      'bake that recipe for the family of bread. Also it will calculate new totals based on the desired amount of bread.' +
+      ' An updated version of this project is being worked on in a private repository.'
+  },
+  {
+    name: 'netTube',
+    url: 'https://github.com/mikeyduece/netTube',
+    description: 'Social media application where users can favorite and add videos to playlists, while being able to friend ' +
+      'others users and see their playlists. Capstone project for the Turing School of Software & Development. ' +
+      'First React project using the YouTube API for proof of concept.'
+  },
+  {
+    name: 'netTube-API',
+    url: 'https://github.com/mikeyduece/netTube-api',
+    description: 'Rails API providing endpoints for the netTube client application. Allowing for self-referential relationships ' +
+      'where users can send, accept, decline friend requests. They can also favorite and/or add videos to custom playlists.'
+  },
+  {
+    name: 'Quantified Self JS API',
+    url: 'https://github.com/mikeyduece/quantified-self-js-api',
+    description: ' JavaScript backend for the quantified-self-client project. Both projects are part of Turing\'s Mod4 curriculum and the backend\'s introduction to JavaScript'
+  },
+  {
+    name: 'Rails Engine',
+    url: 'https://github.com/mikeyduece/rails_engine',
+    description: ' The Rails Engine API allows you to query metadata and perform business analytics on merchants, invoices, invoice items, items, customers, and transactions. The return format for all endpoints is JSON.'
+  },
+  {
+    name: 'Tipsy-iOS13',
+    url: 'https://github.com/mikeyduece/Tipsy-iOS13',
+    description: 'Udemy course application. First intro into the Swift programming language.'
+  }
+]
