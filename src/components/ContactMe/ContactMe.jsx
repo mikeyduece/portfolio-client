44349733a3755cf67f643951e1d0222dcdeaa567
Mike Heft <mikeheft@gmail.com>
2020-04-14 08:24:03 -0600

    Work on materialui form

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,13 +1,112 @@
 // Dependencies
-import React          from 'react'
-import { withNavbar } from '../withNavbar'
+import React, { useState } from 'react'
+import {
+  TextField,
+  Button
+}                          from '@material-ui/core'
+import {
+  Field,
+  reduxForm
+}                          from 'redux-form'
 
-const ContactMe = () => {
+import { withNavbar }        from '../withNavbar'
+import { sendContactMailer } from '../../actions'
+import {
+  theme,
+  useStyles
+}                            from '../theme'
+import { ThemeProvider }     from 'react-bootstrap'
+
+const renderError = ({ error, touched }) => {
+  if (touched && error) {
+    return (
+      <div className='ui error message'>
+        <div className='header'>
+          { error }
+        </div>
+      </div>
+    )
+  }
+}
+
+const renderInput = ({ input, label, meta }) => {
+  const className = `field ${ meta.error && meta.touched ? 'error' : '' }`
+
   return (
-    <div>
-      ContactMe
+    <div className={ className }>
+      <label>{ label }</label>
+      <input { ...input } autoComplete='off' />
+      { renderError(meta) }
     </div>
   )
 }
 
-export default withNavbar(ContactMe)
+const onSubmit = formValues => {
+  sendContactMailer(formValues)
+}
+
+const ContactMe = props => {
+  const { pristine, submitting } = props
+  const classes = useStyles()
+  const [value, setValue] = useState('')
+  const handleChange = (e) => setValue(e.target.value)
+
+  const renderTextField = ({ input, label, meta: { touched, error }, ...custom }) => {
+    return (
+      <TextField errortext={ `${ touched && error }` }
+                 onChange={ handleChange }
+                 { ...input }
+                 { ...custom }
+      />
+    )
+  }
+
+  return (
+    <ThemeProvider theme={ theme }>
+      <div className='d-flex justify-content-center'>
+        <form className='' onSubmit={ onSubmit } autoComplete='off'>
+          <div className='my-3'>
+            <Field name='name' component={ renderTextField } label='Your Name' placeholder='Your Name' />
+          </div>
+          <div className='my-3'>
+            <Field name='email' component={ renderTextField } label='Email' placeholder='Your Email' />
+          </div>
+          <div className='my-3'>
+            <Field name='subject' component={ renderTextField } label='Subject' placeholder='Subject' />
+          </div>
+          <div className='my-3'>
+            <Field name='body' component={ renderTextField } label='Body' multiline rows={ 10 } placeholder='Body'
+                   variant='outlined' />
+          </div>
+          <Button disabled={ pristine || submitting }
+                  color='primary'
+                  variant='outlined'
+                  className={ classes.btnOutline }
+                  size="small"
+                  m='auto'>
+            Submit
+          </Button>
+        </form>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+const validate = values => {
+  const errors = {}
+  const requiredFields = ['name', 'email', 'subject', 'body']
+  requiredFields.forEach(field => {
+    if (!values[field]) {
+      errors[field] = 'Required'
+    }
+  })
+  if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+  return errors
+}
+
+export default reduxForm({
+  form: 'contactMeForm',
+  validate
+})(withNavbar(ContactMe))
---
+++ src/components/theme/index.js
@@ -0,0 +1,32 @@
+import {
+  createMuiTheme,
+  makeStyles
+} from '@material-ui/core/styles'
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#38495a' }
+  },
+  spacing: 3
+})
+
+export const useStyles = makeStyles({
+  root: {
+    minWidth: 200,
+    maxWidth: 200,
+    margin: '0.5em'
+  },
+  container: {
+    display: 'flex'
+  },
+  paper: {
+    margin: theme.spacing(1)
+  },
+  btnOutline: {
+    '&:hover': {
+      color: '#38495a',
+      backgroundColor: '#F6AE2D',
+      cursor: 'pointer'
+    }
+  }
+})
